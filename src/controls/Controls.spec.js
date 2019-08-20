18b4fc2fab673dd84a33eae2799027f0221eed07
cmstexas <crystal.m.soria@gmail.com>
2019-08-20 00:23:38 -0500

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,23 @@
-// Test away!
+import React from 'react'
+import {render, fireEvent} from 'react-testing-library';
+import Controls from './Controls';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+
+it ('renders properly', () => {
+    render(<Controls />)
+});
+
+describe('<Controls />', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Controls />);
+    
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('shows unlicked and open as a default state', () => {
+        const {getByText, queryByText} = render(<Controls />)
+        getByText(/lock gate/i)
+        getByText(/close gate/i)
+    })
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,45 @@
-// Test away!
+import react from 'react';
+import { render, fireEvent} from '@testing-library/react';
+import * as rtl from "react-testing-library";
+import 'jest-dom/extend-expect';
+import Display from './Display';
+import renderer from "react-test-renderer";
+import 'jest-dom/extend-expect';
+afterEach(rtl.cleanup);
+
+
+describe("<Display />", () => {
+    it("matches snapshot", () => {
+        const tree = renderer.create(<Display />);
+
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('shows unlocked and open as default state', () => {
+        const { getByText } = render(<Display />)
+        getByText(/unlocked/i)
+        getByText(/open/i)
+    })
+    it ('State changes w/ props', () => {
+        const { getByText } = render (<Display locked={true} closed={true} />);
+        getByText(/locked/i)
+        getByText(/closed/i)
+    })
+    it('changes red/green based on props truth', () => {
+        const {getByText, rerender} = render(<Display locked={false} ckised={false} />)
+
+        const openDiv = getByText(/open/i);
+        const lockedDiv =  getByText(/unlocked/i);
+        expect(openDiv.className).toBe('led green-led');
+        expect(lockedDiv.className).toBe('led green-led');
+        expect(openDiv.textContent).toBe('Open');    
+        expect(lockedDiv.textContent).toBe('Unlocked');  
+
+        rerender(<Display locked={true} closed={true} />)
+
+        expect (openDiv.className).toBe('led red-led');
+        expect (lockedDiv.className).toBe('led red-led');
+        expect (openDiv.textContent).toBe('Closed');
+        expect (lockedDiv.textContent).toBe('Locked');
+    })
+    });
